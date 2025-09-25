1f5e9712d97ca4bd73b6aa4359bae5e86ab76bb2
JavlonAbdumannopovv <javlonabdumannopovv@gmail.com>
2025-09-25 15:37:10 +0500

    #147. e2e testing

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,9 +1,10 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Review, ReviewDocument } from './review.model';
 import { Model } from 'mongoose';
-import { CreateBookDto } from 'src/books/dto/create-book.dto';
-import { CreateReviewDto, EditReviewDto } from './review.dto';
+import { CreateReviewDto, EditReviewDto } from './dto/review.dto';
+import { Review, ReviewDocument } from './review.model';
 
+@Injectable()
 export class ReviewService {
   constructor(@InjectModel(Review.name) private reviewModel: Model<ReviewDocument>) {}
 
@@ -14,19 +15,22 @@ export class ReviewService {
   }
 
   async deleteReview(reviewId: string) {
-    await this.reviewModel.findByIdAndRemove(reviewId);
+    const isReview = await this.reviewModel.findById(reviewId);
 
-    return reviewId;
+    if (!isReview) throw new NotFoundException('Review with id not found');
+
+    const review = await this.reviewModel.findByIdAndRemove(reviewId);
+
+    return review._id;
   }
 
   async editReview(reviewId: string, dto: EditReviewDto) {
+    console.log(reviewId);
+
     const review = await this.reviewModel.findByIdAndUpdate(
       reviewId,
       {
-        $set: {
-          rating: dto.rating,
-          summary: dto.summary,
-        },
+        $set: { rating: dto.rating, summary: dto.summary },
       },
       { new: true },
     );
@@ -35,8 +39,8 @@ export class ReviewService {
   }
 
   async getReview(courseId: string) {
-    const reviews = await this.reviewModel.find({ course: courseId });
+    const review = await this.reviewModel.find({ course: courseId });
 
-    return reviews; 
+    return review;
   }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,12 +1,29 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { disconnect, Types } from 'mongoose';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { CreateReviewDto, EditReviewDto } from 'src/review/dto/review.dto';
+
+const courseId = new Types.ObjectId().toHexString();
+
+const createReviewDto: CreateReviewDto = {
+  author: new Types.ObjectId().toHexString(),
+  course: courseId,
+  rating: 5,
+  summary: 'Cool',
+};
+
+const editReviewDto: EditReviewDto = {
+  rating: 3,
+  summary: 'Good',
+};
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let createdReviewId: string;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,10 +32,42 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/review/create (POST)', async () => {
     return request(app.getHttpServer())
-      .get('/')
+      .post('/review/create')
+      .send(createReviewDto)
+      .expect(201)
+      .then(({ body }: request.Response) => {
+        createdReviewId = body;
+        expect(createdReviewId).toBeDefined();
+      });
+  });
+
+  it('/review/edit (PUT)', async () => {
+    return request(app.getHttpServer())
+      .put('/review/edit/' + createdReviewId)
+      .send(createReviewDto)
       .expect(200)
-      .expect('Hello World!');
+      .then(({ body }: request.Response) => {
+        createdReviewId = body;
+        expect(createdReviewId).toBeDefined();
+      });
+  });
+
+  it('/review/get (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/review/get/' + courseId)
+      .expect(200);
+  });
+
+  it('/review/delete (DELETE)', () => {
+    return request(app.getHttpServer())
+      .delete('/review/delete/' + createdReviewId)
+      .expect(200);
+  });
+
+  afterAll(async () => {
+    await app.close();
+    await disconnect();
   });
 });
